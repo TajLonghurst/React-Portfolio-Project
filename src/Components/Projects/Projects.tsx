936ad46295493088aa42c73b7373cc716205ee38
Tajinboxx <TajLonghurstsoft@gmail.com>
2022-11-18 20:00:20 +1300

    Update Projects.tsx

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -4,19 +4,20 @@ import ProjectImgsMobile from "./ProjectImgsMobile";
 import ProjectNav from "./ProjectNav";
 import classes from "./Projects.module.scss";
 import useWindowSize from "../../Hooks/use-windowSize";
+import { AnimatePresence } from "framer-motion";
 
 const Projects = () => {
   const { isSmallDesktopView } = useWindowSize();
 
-  const container = isSmallDesktopView
-    ? classes.container
-    : classes.containermobile;
+  const container = isSmallDesktopView ? classes.container : classes.containermobile;
   return (
     <section className={container}>
       <div className={classes.row}>
         {!isSmallDesktopView && <ProjectImgsMobile />}
         {isSmallDesktopView && <ProjectNav />}
-        {isSmallDesktopView && <ProjectImgs />}
+        <AnimatePresence initial={true} exitBeforeEnter={true} onExitComplete={() => null}>
+          {isSmallDesktopView && <ProjectImgs />}
+        </AnimatePresence>
       </div>
     </section>
   );
